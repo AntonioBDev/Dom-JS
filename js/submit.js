5509a73196bdf0dde226c4df452e84bdbe5bb656
BjoseAntonio <bas.99jose@gmail.com>
2025-01-29 16:46:00 -0600

    Cambios HTML y JS

--- js/submit.js
+++ js/submit.js
@@ -29,12 +29,12 @@ formulario.addEventListener('submit', function(evento){
 
     //validar formulario 
      if(nombre === '' || apellidos === '' || email === '' || comentario === ''){
-        mensajeError('Es necesario llenar todo los campos');
+        mostrarAlerta('Es necesario llenar todo los campos','error');
         return;
     }
 
     //Enviar formulario 
-    enviarFormulario('Enviando Formulario');
+    mostrarAlerta('Enviando Formulario');
 
     //Reset Formulario
 });
@@ -46,30 +46,48 @@ function leerText(e){
     console.log(datos);
 }
 
+//Refactorización 
+function mostrarAlerta(mensaje, error = null){
+    const elementoP = document.createElement('P');
+    elementoP.textContent = mensaje;
+
+
+    if(error === "error"){
+        elementoP.classList.add('error');
+    }else{
+        elementoP.classList.add('notValCor');
+    }
+
+    fieldset.appendChild(elementoP)
+    setTimeout(()=>{
+        elementoP.remove();
+    },3000)
+}
+
 function mensajeError(mensaje){
     // console.log(mensaje);
     //Mostrar en pantalla 
     //crear elemento P 
-    const errorP = document.createElement('P');
-    errorP.textContent = mensaje;
-    errorP.classList.add('error');
-    fieldset.appendChild(errorP);
+    //const errorP = document.createElement('P');
+    //errorP.textContent = mensaje;
+    //errorP.classList.add('error');
+    //fieldset.appendChild(errorP);
 
-    setTimeout(()=>{
-        errorP.remove();
-    },3000)
+    // setTimeout(()=>{
+    //     errorP.remove();
+    // },3000)
 }
 
 function enviarFormulario(mensaje){
-    const enviarPresentacion = document.createElement('P');
-    enviarPresentacion.textContent = mensaje;
-    enviarPresentacion.classList.add('notValCor');
+    //const enviarPresentacion = document.createElement('P');
+   // enviarPresentacion.textContent = mensaje;
+   // enviarPresentacion.classList.add('notValCor');
 
-    fieldset.appendChild(enviarPresentacion);
+    //fieldset.appendChild(enviarPresentacion);
 
-    setTimeout(()=>{
-        enviarPresentacion.remove();
-    },3000)
+    // setTimeout(()=>{
+    //     enviarPresentacion.remove();
+    // },3000)
 
 }
 
